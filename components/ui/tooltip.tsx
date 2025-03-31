6a647e783f579e7891a861cc8f620b7e31fd802a
inogai <inogai.sign@gmail.com>
2025-03-31 16:13:56 +0800

    style: run lint:fix

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -1,13 +1,13 @@
-import * as TooltipPrimitive from '@rn-primitives/tooltip';
-import * as React from 'react';
-import { Platform, StyleSheet } from 'react-native';
-import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
-import { TextClassContext } from '~/components/ui/text';
-import { cn } from '~/lib/utils';
+import * as TooltipPrimitive from '@rn-primitives/tooltip'
+import * as React from 'react'
+import { Platform, StyleSheet } from 'react-native'
+import Animated, { FadeIn, FadeOut } from 'react-native-reanimated'
+import { TextClassContext } from '~/components/ui/text'
+import { cn } from '~/lib/utils'
 
-const Tooltip = TooltipPrimitive.Root;
+const Tooltip = TooltipPrimitive.Root
 
-const TooltipTrigger = TooltipPrimitive.Trigger;
+const TooltipTrigger = TooltipPrimitive.Trigger
 
 const TooltipContent = React.forwardRef<
   TooltipPrimitive.ContentRef,
@@ -19,13 +19,21 @@ const TooltipContent = React.forwardRef<
         entering={Platform.select({ web: undefined, default: FadeIn })}
         exiting={Platform.select({ web: undefined, default: FadeOut })}
       >
-        <TextClassContext.Provider value='text-sm native:text-base text-popover-foreground'>
+        <TextClassContext.Provider value="text-sm native:text-base text-popover-foreground">
           <TooltipPrimitive.Content
             ref={ref}
             sideOffset={sideOffset}
             className={cn(
-              'z-50 overflow-hidden rounded-md border border-border bg-popover px-3 py-1.5 shadow-md shadow-foreground/5 web:animate-in web:fade-in-0 web:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-              className
+              `
+                z-50 overflow-hidden rounded-md border border-border bg-popover
+                px-3 py-1.5 shadow-md shadow-foreground/5
+                data-[side=bottom]:slide-in-from-top-2
+                data-[side=left]:slide-in-from-right-2
+                data-[side=right]:slide-in-from-left-2
+                data-[side=top]:slide-in-from-bottom-2
+                web:animate-in web:fade-in-0 web:zoom-in-95
+              `,
+              className,
             )}
             {...props}
           />
@@ -33,7 +41,7 @@ const TooltipContent = React.forwardRef<
       </Animated.View>
     </TooltipPrimitive.Overlay>
   </TooltipPrimitive.Portal>
-));
-TooltipContent.displayName = TooltipPrimitive.Content.displayName;
+))
+TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
-export { Tooltip, TooltipContent, TooltipTrigger };
+export { Tooltip, TooltipContent, TooltipTrigger }
