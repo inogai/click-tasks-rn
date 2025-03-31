5e19dbf934677f9b39fa0a6fcde00d5dffd0134c
inogai <inogai.sign@gmail.com>
2025-03-31 16:12:01 +0800

    chore: configure eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import antfu from '@antfu/eslint-config'
+import readableTailwind from 'eslint-plugin-readable-tailwind'
+import tailwind from 'eslint-plugin-tailwindcss'
+
+export default antfu(
+  {
+    react: true,
+  },
+  {
+    plugins: {
+      'tailwindcss': tailwind,
+      'readable-tailwind': readableTailwind,
+    },
+    rules: {
+    // 'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/enforces-negative-arbitrary-values': 'warn',
+      'tailwindcss/enforces-shorthand': 'warn',
+      'tailwindcss/migration-from-tailwind-2': 'warn',
+      'tailwindcss/no-arbitrary-value': 'off',
+      'tailwindcss/no-custom-classname': 'warn',
+      'tailwindcss/no-contradicting-classname': 'error',
+      'tailwindcss/no-unnecessary-arbitrary-value': 'warn',
+
+      'readable-tailwind/multiline': 'warn',
+      'readable-tailwind/no-unnecessary-whitespace': 'warn',
+      'readable-tailwind/sort-classes': 'warn',
+      'readable-tailwind/no-duplicate-classes': 'warn',
+    },
+  },
+)
