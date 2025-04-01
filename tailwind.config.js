e70b93c0713216a5cc919ffb1f0395045c7143af
inogai <inogai.sign@gmail.com>
2025-04-01 15:46:09 +0800

    feat: colorscheme trust blue and finance green

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        finance: {
+          DEFAULT: 'hsl(var(--finance))',
+          foreground: 'hsl(var(--finance-foreground))',
+        },
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
