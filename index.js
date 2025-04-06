820fe3ce3f92a68d54c3781063d72945c23de682
inogai <inogai.sign@gmail.com>
2025-04-06 16:49:47 +0800

    feat: add crypto.getRandomValues polyfill via side-effect import

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { registerRootComponent } from 'expo'
 import { ExpoRoot } from 'expo-router'
 
+import 'react-native-get-random-values'
+
 // https://docs.expo.dev/router/reference/troubleshooting/#expo_router_app_root-not-defined
 
 // Must be exported or Fast Refresh won't update the context
