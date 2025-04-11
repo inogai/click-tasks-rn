9c674f81532ddc69273669b0270fc37051140cb6
inogai <inogai.sign@gmail.com>
2025-04-11 17:46:22 +0800

    feat: add status and make task form a component

--- app/taskadd.tsx
+++ app/taskadd.tsx
@@ -1,36 +1,19 @@
 import type { SubmitHandler } from 'react-hook-form'
 import type { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
+import type { taskZod } from '~/lib/realm'
 import { useRealm } from '@realm/react'
-import { router } from 'expo-router'
-import { CheckIcon } from 'lucide-nativewind'
-import { useEffect } from 'react'
-import { useForm, useWatch } from 'react-hook-form'
 
+import { router } from 'expo-router'
 import {
   SafeAreaView,
-  Text,
 } from 'react-native'
-import { FormField } from '~/components/form/form-field'
-import { Button } from '~/components/ui/button'
-import { TaskRecord, TaskStatus, taskZod } from '~/lib/realm'
+import { TaskForm } from '~/components/task-form'
+import { TaskRecord, TaskStatus } from '~/lib/realm'
 
 type FormData = z.infer<typeof taskZod>
 
 export default function Screen() {
   const realm = useRealm()
-
-  const {
-    control,
-    handleSubmit,
-    formState: { errors, isValid, isSubmitting },
-    trigger,
-  } = useForm<FormData>({
-    resolver: zodResolver(taskZod),
-    defaultValues: {
-      status: TaskStatus.PENDING,
-    },
-  })
 
   const onSubmit: SubmitHandler<FormData> = (data) => {
     const task = TaskRecord.create(data)
@@ -43,67 +26,14 @@ export default function Screen() {
     router.back()
   }
 
-  // trigger validation on every field change
-  const watched = useWatch({ control })
-  useEffect(() => {
-    trigger()
-  }, [trigger, watched])
-
   return (
     <SafeAreaView>
-      <FormField
-        control={control}
-        name="summary"
-        label="Task Summary"
-        placeholder="Enter task summary"
-        className="mb-4"
+      <TaskForm
+        defaultValues={{
+          status: TaskStatus.PENDING,
+        }}
+        onSubmit={onSubmit}
       />
-
-      <FormField
-        control={control}
-        name="venue"
-        label="Venue"
-        placeholder="Enter venue (optional)"
-        className="mb-4"
-      />
-
-      <FormField
-        control={control}
-        name="due"
-        label="Due Date"
-        type="datetime"
-        className="mb-4"
-      />
-
-      <FormField
-        control={control}
-        name="plannedBegin"
-        label="Planned Begin Date"
-        type="datetime"
-        className="mb-4"
-      />
-
-      <FormField
-        name="plannedEnd"
-        label="Planned End Date"
-        type="datetime"
-        control={control}
-        className="mb-4"
-      />
-
-      <Text className="text-destructive">
-        { errors.root?.message }
-      </Text>
-
-      <Button
-        className="mt-4 flex-row gap-4"
-        onPress={handleSubmit(onSubmit)}
-        disabled={!isValid || isSubmitting}
-      >
-        <CheckIcon className="text-primary-foreground" />
-        <Text className="text-primary-foreground">Submit</Text>
-      </Button>
-
     </SafeAreaView>
   )
 }
---
+++ components/task-form.tsx
@@ -0,0 +1,111 @@
+import type { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { CheckIcon } from 'lucide-nativewind'
+import { useEffect } from 'react'
+import { useForm, useWatch } from 'react-hook-form'
+
+import {
+  Text,
+  View,
+} from 'react-native'
+import { FormField } from '~/components/form/form-field'
+import { SelectField } from '~/components/form/select-field'
+import { Button } from '~/components/ui/button'
+import { TaskStatus, taskZod } from '~/lib/realm'
+
+type FormData = z.infer<typeof taskZod>
+
+interface TaskFormProps {
+  defaultValues?: Partial<FormData>
+  onSubmit: (data: FormData) => void
+}
+
+export function TaskForm({
+  defaultValues,
+  onSubmit,
+}: TaskFormProps) {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitting },
+    trigger,
+  } = useForm<FormData>({
+    resolver: zodResolver(taskZod),
+    defaultValues,
+  })
+
+  // trigger validation on every field change
+  const watched = useWatch({ control })
+  useEffect(() => {
+    trigger()
+  }, [trigger, watched])
+
+  return (
+    <View>
+      <FormField
+        control={control}
+        name="summary"
+        label="Task Summary"
+        placeholder="Enter task summary"
+        className="mb-4"
+      />
+
+      <FormField
+        control={control}
+        name="venue"
+        label="Venue"
+        placeholder="Enter venue (optional)"
+        className="mb-4"
+      />
+
+      <FormField
+        control={control}
+        name="due"
+        label="Due Date"
+        type="datetime"
+        className="mb-4"
+      />
+
+      <FormField
+        control={control}
+        name="plannedBegin"
+        label="Planned Begin Date"
+        type="datetime"
+        className="mb-4"
+      />
+
+      <FormField
+        name="plannedEnd"
+        label="Planned End Date"
+        type="datetime"
+        control={control}
+        className="mb-4"
+      />
+
+      <SelectField
+        name="status"
+        label="Status"
+        control={control}
+        className="mb-4"
+        options={[
+          { label: 'Pending', value: TaskStatus.PENDING },
+          { label: 'Completed', value: TaskStatus.COMPLETED },
+        ]}
+      />
+
+      <Text className="text-destructive">
+        { errors.root?.message }
+      </Text>
+
+      <Button
+        className="mt-4 flex-row gap-4"
+        onPress={handleSubmit(onSubmit)}
+        disabled={!isValid || isSubmitting}
+      >
+        <CheckIcon className="text-primary-foreground" />
+        <Text className="text-primary-foreground">Submit</Text>
+      </Button>
+
+    </View>
+  )
+}
