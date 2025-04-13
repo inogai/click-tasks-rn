adb13805133141fcff04b2133ef830a030a7c7c9
inogai <inogai.sign@gmail.com>
2025-04-13 12:56:21 +0800

    ref(index.js): replace jsx with createElement call

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { registerRootComponent } from 'expo'
 import { ExpoRoot } from 'expo-router'
+import { createElement } from 'react'
 
 import 'react-native-get-random-values'
 
@@ -8,7 +9,7 @@ import 'react-native-get-random-values'
 // Must be exported or Fast Refresh won't update the context
 export function App() {
   const ctx = require.context('./app')
-  return <ExpoRoot context={ctx} />
+  return createElement(ExpoRoot, { context: ctx })
 }
 
 registerRootComponent(App)
