da8ee59942d4da0689a3b42ed2dfd7fb8b75b141
inogai <inogai.sign@gmail.com>
2025-04-13 20:46:02 +0800

    ref: create a variant of View that accepts 'textClass'

--- components/ui/text.tsx
+++ components/ui/text.tsx
@@ -1,12 +1,28 @@
-import type { SlottableTextProps, TextRef } from '@rn-primitives/types'
+import type { SlottableTextProps, TextRef, ViewRef } from '@rn-primitives/types'
+import type { ViewProps as RNViewProps } from 'react-native'
 
 import * as Slot from '@rn-primitives/slot'
 import * as React from 'react'
-import { Text as RNText } from 'react-native'
+import { Text as RNText, View as RNView } from 'react-native'
 
 import { cn } from '~/lib/utils'
 
 const TextClassContext = React.createContext<string | undefined>(undefined)
+
+interface ViewProps extends RNViewProps {
+  textClass?: string
+}
+
+const View = React.forwardRef<ViewRef, ViewProps & { textClass?: string }>(
+  ({ textClass, ...props }, ref) => {
+    const parentTextClass = React.useContext(TextClassContext)
+    return (
+      <TextClassContext.Provider value={cn(parentTextClass, textClass)}>
+        <RNView {...props} ref={ref} />
+      </TextClassContext.Provider>
+    )
+  },
+)
 
 const Text = React.forwardRef<TextRef, SlottableTextProps>(
   ({ className, asChild = false, ...props }, ref) => {
@@ -26,4 +42,6 @@ const Text = React.forwardRef<TextRef, SlottableTextProps>(
 )
 Text.displayName = 'Text'
 
-export { Text, TextClassContext }
+export type { ViewProps }
+
+export { Text, TextClassContext, View }
