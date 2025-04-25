8fae85f5392fd6156d96a0be0d85425727cad5af
inogai <inogai.sign@gmail.com>
2025-04-25 16:33:39 +0800

    fix(app-header): proper layout for header

--- components/app-header.tsx
+++ components/app-header.tsx
@@ -5,38 +5,26 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { AppLogo } from '~/components/app-logo'
 import { ThemeToggle } from '~/components/theme-toggle'
 import { Button } from '~/components/ui/button'
-import { Text } from '~/components/ui/text'
 
 import { MenuIcon } from '~/lib/icons'
 
-function render<Props>(obj: undefined | string | React.FC<Props>, props: Props) {
-  if (obj === undefined) {
-    return obj
-  }
-
-  if (typeof obj === 'string') {
-    return <Text>{obj}</Text>
-  }
-
-  return obj(props)
-}
-
 export function AppHeader() {
   return (
-    <SafeAreaView className={`
-      h-28 w-full flex-row items-center justify-between border-b border-border
-      p-4
-    `}
+    <SafeAreaView
+      edges={['top', 'left', 'right']}
+      className={`
+        flex-row items-center justify-between border-b border-border p-4
+      `}
     >
-      <View className="flex-1 basis-0 items-start">
+      <View className="flex-1 basis-0 flex-row justify-start gap-2">
         <Button size="icon" variant="ghost">
           <MenuIcon />
         </Button>
       </View>
 
-      <AppLogo className="-my-4" />
+      <AppLogo />
 
-      <View className="flex-1 basis-0 items-end">
+      <View className="flex-1 basis-0 flex-row justify-end gap-2">
         <ThemeToggle />
       </View>
     </SafeAreaView>
