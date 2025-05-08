8139af1b6b6dd77e685b8e4a4e56094b86e0780c
inogai <inogai.sign@gmail.com>
2025-05-08 22:10:22 +0800

    feat(countdown-view): use flashlist to allow scroll

--- components/views/countdown-view.tsx
+++ components/views/countdown-view.tsx
@@ -1,3 +1,4 @@
+import { FlashList } from '@shopify/flash-list'
 import { differenceInCalendarDays } from 'date-fns'
 import { router } from 'expo-router'
 import { Trans } from 'react-i18next'
@@ -21,55 +22,64 @@ export function CountdownView() {
   }
 
   return (
-    <View className="w-full">
+    <View className="h-28 w-full">
       <Card>
         <CardHeader className={`
-          w-full flex-row items-center justify-between gap-6
+          w-full flex-row items-center justify-between gap-6 p-0
         `}
         >
-          {countdowns.map((countdown, idx) => {
-            const daysLeft = differenceInCalendarDays(
-              countdown.taskRecord.due!,
-              new Date(),
-            )
-            console.log('Countdown:', countdown.taskRecord.summary, 'Days left:', daysLeft)
-            const numberClass = cn(
-              'text-3xl font-bold',
-              daysLeft <= 7 && 'text-yellow-500',
-              daysLeft <= 1 && 'text-red-500',
-            )
+          <FlashList
+            contentContainerStyle={{ padding: 24 }}
+            data={Array.from(countdowns)}
+            estimatedItemSize={119} // size for w-28, since we don't care about optimization for len < 3
+            ItemSeparatorComponent={() => <Separator className="mx-6" orientation="vertical" />}
+            keyExtractor={countdown => countdown._id.toHexString()}
+            horizontal
+            renderItem={({ item: countdown }) => {
+              const daysLeft = differenceInCalendarDays(
+                countdown.taskRecord.due!,
+                new Date(),
+              )
+              const numberClass = cn(
+                'text-3xl font-bold',
+                daysLeft <= 7 && 'text-yellow-500',
+                daysLeft <= 1 && 'text-red-500',
+              )
 
-            return (
-              <>
-                {idx !== 0 && <Separator orientation="vertical" />}
-                <Pressable
-                  className="flex-1"
-                  key={countdown._id.toHexString()}
-                  onPress={() => {
-                    router.push({
-                      pathname: '/task/update/[taskId]',
-                      params: {
-                        taskId: countdown.taskRecord._id.toHexString(),
-                      },
-                    })
-                  }}
-                >
-                  <Text className="line-clamp-1 font-medium">{countdown.taskRecord.summary}</Text>
-                  <Text className={`
-                    line-clamp-1 w-full justify-end font-medium
-                    text-muted-foreground
-                  `}
+              return (
+                <>
+                  <Pressable
+                    className={cn(
+                      'w-56',
+                      countdowns.length >= 2 && 'w-40',
+                      countdowns.length >= 3 && 'w-28',
+                    )}
+                    onPress={() => {
+                      router.push({
+                        pathname: '/task/update/[taskId]',
+                        params: {
+                          taskId: countdown.taskRecord._id.toHexString(),
+                        },
+                      })
+                    }}
                   >
-                    <Trans
-                      components={{ 1: <Text className={numberClass} /> }}
-                      i18nKey="countdown_view.days_left"
-                      values={{ count: daysLeft }}
-                    />
-                  </Text>
-                </Pressable>
-              </>
-            )
-          })}
+                    <Text className="line-clamp-1 font-medium">{countdown.taskRecord.summary}</Text>
+                    <Text className={`
+                      line-clamp-1 w-full justify-end font-medium
+                      text-muted-foreground
+                    `}
+                    >
+                      <Trans
+                        components={{ 1: <Text className={numberClass} /> }}
+                        i18nKey="countdown_view.days_left"
+                        values={{ count: daysLeft }}
+                      />
+                    </Text>
+                  </Pressable>
+                </>
+              )
+            }}
+          />
         </CardHeader>
       </Card>
     </View>
